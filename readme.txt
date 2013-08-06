960bec92b497c3effe7934e351ed14dfe79f7b3e
Kevin Langley Jr <klangley@voceconnect.com>
2013-08-05 23:38:33 -0400

    initial commit

---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Voce Post Meta PSU ===
+
+Contributors: klangley, voceplatforms
+Tags: meta, psu, post, selection
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Extend Voce Post Meta with post selection fields
+
+== Description ==
+
+Extend Voce Post Meta with [post selection ui](http://github.com/voceconnect/post-selection-ui) inputs
+
+== Installation ==
+
+=== As standard plugin: ===
+> See [Installing Plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins).
+
+=== As theme or plugin dependency: ===
+> After dropping the plugin into the containing theme or plugin, add the following:
+```php
+if( ! class_exists( 'Voce_Post_Meta_Post_Selection_UI' ) ) {
+	require_once( $path_to_voce_post_meta_psu . '/voce-post-meta-psu.php' );
+}
+```
+
+== Usage ==
+
+==== Example ====
+
+```php
+<?php
+	add_metadata_field( 'post_selection_ui_group', 'post_selection_ui_key', 'Title', 'psu', array( 'post_type' => 'custom_post_type', 'post_status' => 'publish', 'limit' => 3 ) );
+?>
+```
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ voce-post-meta-psu.php
@@ -0,0 +1,69 @@
+<?php
+/*
+  Plugin Name: Voce Meta Post Selection UI
+  Plugin URI: http://vocecommunications.com
+  Description: Extends Voce Post Meta with a Post Selection UI
+  Version: 0.1
+  Author: kevinlangleyjr
+  Author URI: http://vocecommunications.com
+  License: GPL2
+ */
+
+class Voce_Post_Meta_Post_Selection_UI {
+
+	public static function initialize() {
+		if( !class_exists( 'Post_Selection_UI' ) ){
+			_doing_it_wrong( __METHOD__, __( 'This plugin requires the Post Selection UI plugin to already be loaded' ), '3.5' );
+			return;
+		}
+
+		add_filter( 'meta_type_mapping', array( __CLASS__, 'meta_type_mapping' ) );
+	}
+
+	/**
+	 * @method meta_type_mapping
+	 * @param type $mapping
+	 * @return array
+	 */
+	public static function meta_type_mapping( $mapping ) {
+		$mapping['psu'] = array(
+			'class' => 'Voce_Meta_Field',
+			'args' => array(
+				'display_callbacks' => array( 'voce_post_selection_ui_meta_field_display' ),
+				'sanitize_callbacks' => array( function($field, $old_value, $new_value, $post_id){
+					return $new_value;
+				} )
+			)
+		);
+		return $mapping;
+	}
+
+}
+
+if ( class_exists( 'Voce_Meta_API' ) ) {
+	Voce_Post_Meta_Post_Selection_UI::initialize();
+
+	function voce_post_selection_ui_meta_field_display( $field, $value, $post_id ) {
+		$args = array_merge( array(
+			'id' => $field->get_input_id(),
+			'selected' => (array) explode( ',', $value ),
+		), (array) $field->args );
+		?>
+		<div class="voce-post-meta-psu-container">
+			<div class="label">
+				<?php voce_field_label_display( $field ); ?>
+			</div>
+			<div class="form-input">
+				<div class="widget">
+					<div class="widget-inside" style="display:block;">
+						<?php
+							echo post_selection_ui( $field->get_name(), $args );
+							echo ( isset( $args['description'] ) ) ? '<p class="description">' . $args['description'] . '</p>' : '';
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+}
